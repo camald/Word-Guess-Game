774d26f870b19a02a221e82a589af5bf83ed205b
camald <43049864+camald@users.noreply.github.com>
2018-10-13 23:47:13 -0400

    Update script.js

---
+++ assets/JS/script.js
@@ -0,0 +1,25 @@
+main = {};
+main.wordarry = []
+main.wordarry = []
+
+main.lives =4;
+main.numinwordbark = word.length;
+
+main.word = "test";
+main.wordU = "";
+
+main.pullword = function(){
+    main.word=word.list [Math.floor(math.random)]
+}
+main.setunderline = function(){
+    main.pullword();
+    for(i=0; i<main,word.length; i++){
+        main.array[i] = main.word.charAt(i);
+main.wordarray[i] = "";
+  }
+main.wordU =main.wordUwarry.join("");
+document.getElementById("word").innerHTML = main.wordU;
+document.getElementById("numletters").innerHTML = main.word.length;
+
+}
+main.setunderline();
